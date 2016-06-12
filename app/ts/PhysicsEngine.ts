d0f01194544f38bba39da9e4fdaccdd9dad1ae3b
Jesse Kramer <jessekramer@Jesses-iMac.local>
2016-06-12 19:50:35 +0200

    fixed clearframe bug, added isHit method to physicsengine

--- app/ts/PhysicsEngine.ts
+++ app/ts/PhysicsEngine.ts
@@ -10,4 +10,26 @@ export class PhysicsEngine {
       gameObject.move(dt)
     })
   }
+
+  public isHit(a: GameObject, b: GameObject): boolean {
+    let aMaxX = a.getPosition().getX() + a.getSize().getX() / 2
+    let aMinX = a.getPosition().getX() - a.getSize().getX() / 2
+    let aMaxY = a.getPosition().getY() + a.getSize().getY() / 2
+    let aMinY = a.getPosition().getY() - a.getSize().getY() / 2
+
+    let bMaxX = b.getPosition().getX() + b.getSize().getX() / 2
+    let bMinX = b.getPosition().getX() - b.getSize().getX() / 2
+    let bMaxY = b.getPosition().getY() + b.getSize().getY() / 2
+    let bMinY = b.getPosition().getY() - b.getSize().getY() / 2
+    
+    return (
+      (
+        ((aMaxX > bMinX) && (bMaxX > aMinX)) ||
+        ((aMinX > bMinX) && (bMaxX > aMinX))
+      ) && (
+        ((aMaxY > bMinY) && (bMaxY > aMinY)) ||
+        ((aMinY > bMinY) && (bMaxY > aMinY))
+      )
+    )
+  }
 }
